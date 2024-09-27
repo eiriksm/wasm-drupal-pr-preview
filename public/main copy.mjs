e64c932078a009c0a2d750233334a5d10987a08c
Eirik S. Morland <eirik@morland.no>
2024-09-27 07:24:29 +0200

    Remove some duplicate files

--- public/main copy.mjs
+++
@@ -1,8 +0,0 @@
-import { registerWorker } from './utils.mjs'
-import { defineTrialManagerElement } from "./trial-manager.mjs";
-
-defineTrialManagerElement()
-registerWorker(
-  `${window.location.origin}/service-worker.mjs`,
-  `${window.location.origin}/service-worker.js`
-)
